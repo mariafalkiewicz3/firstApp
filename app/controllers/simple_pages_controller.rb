e5733112b4e058c46461d99326a1a4b34b1c00b2
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-06-08 01:22:26 +0000

    heroku contact

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,14 +2,14 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
-end
 
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
       to: 'mariafalkiewicz3@gmail.com',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
+  end
 end
