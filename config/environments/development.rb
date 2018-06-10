f12849805f2c45fce0a4a518148d9e2e847ae99e
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-06-10 05:48:56 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.default_url_options = { host: 'https://28a148f303314b1f9a3ee9a25c0adfff.vfs.cloud9.us-east-2.amazonaws.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do 
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  resources :users
   resources :products
-  root 'simple_pages#landing_page'
-  get 'simple_pages/about'
-  get 'simple_pages/contact'
-  get '/products', to: 'products#index'
-  get '/orders', to: 'orders#index'
-  post 'simple_pages/thank_you'
-end
-
-Rails.application.routes.draw do 
   resources :orders, only: [:index, :show, :create, :destroy]
   root 'simple_pages#landing_page'
   get 'simple_pages/about'
