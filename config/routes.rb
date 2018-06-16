dcd869c899b9a9984e0305a6b29f8a5fa070ef46
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-06-16 01:46:41 +0000

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end
   resources :orders, only: [:index, :show, :create, :destroy]
   root 'simple_pages#landing_page'
   get 'simple_pages/about'
