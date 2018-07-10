e3b273b7b6e0c47cf5ec839110bf19875efbb0b8
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-07-10 22:51:10 +0000

    5.11

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ApplicationMailer
             to: 'mariafalkiewicz3@gmail.com',
             subject: "A new contact form message from #{name}")
     end
+    def welcome(user)
+        @appname = "LifeOnTour"
+        mail(to: user.email,
+            subject: "Welcome to #{@appname}!")
+    end
 end
