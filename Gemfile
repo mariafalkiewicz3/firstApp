19d52b7e530088dbed777ce498a4dbea94833cac
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-08-17 13:18:11 -0600

    6.3

--- Gemfile
+++ Gemfile
@@ -69,4 +69,6 @@ gem 'devise'
 
 gem 'cancancan', '~> 1.10'
 
+gem 'rails-controller-testing'
+
 gem 'will_paginate', '~> 3.1.0'
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+
+  before do
+	   @user = User.create!(email: 'yo@email.com', password: 'fedcba')
+	   @user2 = User.create!(email: 'yo2@email.com', password: 'rsdfcba')
+  end
+
+  describe 'GET #show' do
+    context 'when a user is logged in' do
+      before do
+        sign_in @user
+      end
+
+      it 'loads correct user details' do
+        get :show, params: {id: @user.id}
+        expect(response).to be_ok
+        expect(assigns(:user)).to eq @user
+      end
+      it 'cannot access other users show page' do
+        get :show, params: { id: @user2.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when a user is not logged in' do
+      it 'redirects to login' do
+        get :show, params: { id: @user.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+  end
+end
