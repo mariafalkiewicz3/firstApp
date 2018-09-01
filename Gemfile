95f708eff0eaf39e1ac2949283f5fe20ce8316ba
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-09-01 13:57:54 -0600

    breakman

--- Gemfile
+++ Gemfile
@@ -76,4 +76,6 @@ gem 'will_paginate', '~> 3.1.0'
 
 gem 'stripe'
 
+gem 'brakeman'
+
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,6 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    byebug
     @product = Product.new
   end
 
