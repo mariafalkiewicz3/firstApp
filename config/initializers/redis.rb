95e823f191257a44428016ed4b499cfc90c9370b
mariafalkiewicz3 <mariafalkiewicz3@gmail.com>
2018-09-27 18:10:04 -0600

    6.15

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  $redis = Redis.new(host: ENV["REDIS_URL"])
+  $redis = Redis.new(host: ENV["redis://h:p5545f175b864e8ee3ef478a7e3f8fde9b68f8868115deff1a316976b9689267f@ec2-54-210-98-224.compute-1.amazonaws.com:22059"])
 else
   $redis = Redis.new(host: 'localhost', port: 6379)
 end
